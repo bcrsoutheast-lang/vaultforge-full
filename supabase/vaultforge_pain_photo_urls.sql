fe1f79049668c9fac39d34e616ad092f4c00ac84
bcrsoutheast-lang <bcrsoutheast@gmail.com>
2026-05-07 21:14:44 -0400

    Delete supabase/vaultforge_pain_photo_urls.sql

--- supabase/vaultforge_pain_photo_urls.sql
+++
@@ -1,4 +0,0 @@
--- VaultForge Pain Button photo support
--- Safe additive column only. Does not modify existing records or policies.
-alter table public.vf_pain_submissions
-add column if not exists photo_urls text[] default array[]::text[];
