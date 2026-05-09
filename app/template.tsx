dca1475262ff6b436c2cf98c3b485e7001ad8b06
bcrsoutheast-lang <bcrsoutheast@gmail.com>
2026-05-09 12:53:56 -0400

    Delete app/template.tsx

--- app/template.tsx
+++
@@ -1,6 +0,0 @@
-
-import VaultForgeGlobalMemberNavGate from "./components/VaultForgeGlobalMemberNavGate";
-
-export default function Template({ children }: { children: React.ReactNode }) {
-  return <VaultForgeGlobalMemberNavGate>{children}</VaultForgeGlobalMemberNavGate>;
-}
