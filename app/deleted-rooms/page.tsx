2db430e08339efb48e514e33d361e303a4e0abd3
bcrsoutheast-lang <bcrsoutheast@gmail.com>
2026-05-17 11:42:46 -0400

    Add files via upload

--- app/deleted-rooms/page.tsx
+++ app/deleted-rooms/page.tsx
@@ -1,5 +1,14 @@
 import VaultForgeRoomFolderPage from "../components/VaultForgeRoomFolderPage";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
 export default function DeletedRoomsPage() {
-  return <VaultForgeRoomFolderPage status="deleted" />;
+  return (
+    <VaultForgeRoomFolderPage
+      status="deleted"
+      title="Deleted Rooms."
+      subtitle="Hidden rooms removed from active workflow. Restore or permanently delete."
+    />
+  );
 }
