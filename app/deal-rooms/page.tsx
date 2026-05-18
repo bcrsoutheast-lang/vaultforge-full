5bbfd2ac6f6b6212ac5f9a084133735bd5cb5fa3
bcrsoutheast-lang <bcrsoutheast@gmail.com>
2026-05-18 10:08:51 -0400

    Add files via upload

---
+++ app/deal-rooms/page.tsx
@@ -0,0 +1,8 @@
+import VaultForgeDealRoomsClient from "../components/VaultForgeDealRoomsClient";
+
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default function DealRoomsPage() {
+  return <VaultForgeDealRoomsClient />;
+}
