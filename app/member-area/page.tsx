d153c709eb464239c7470ecec9d4d1070a54ba24
bcrsoutheast-lang <bcrsoutheast@gmail.com>
2026-05-22 13:39:04 -0400

    Add files via upload

--- app/member-area/page.tsx
+++ app/member-area/page.tsx
@@ -1,1 +1,8 @@
+import { redirect } from "next/navigation";
 
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+
+export default function MemberAreaPage() {
+  redirect("/member-controlled-threads");
+}
