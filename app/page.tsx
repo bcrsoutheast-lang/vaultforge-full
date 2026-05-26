dbe69af95e838e3ad64343b5d89c3d27c3492c04
bcrsoutheast-lang <bcrsoutheast@gmail.com>
2026-05-26 15:57:37 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,2 @@
-import { createServerClient } from '@supabase/ssr'
-import { cookies } from 'next/headers'
-import { redirect } from 'next/navigation'
-export default async function Home() {
-const cookieStore = cookies()
-const supabase = createServerClient(process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!, { cookies: { get(name: string) { return cookieStore.get(name)?.value } } })
-const { data: { session } } = await supabase.auth.getSession()
 if (!session) redirect('/login')
 redirect('/message-command')
-}
