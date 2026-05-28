4ff63701a87e1309df38fe41f7e4a995e5fd5d88
bcrsoutheast-lang <bcrsoutheast@gmail.com>
2026-05-27 20:45:03 -0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,4 +3,7 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey)
+// This prevents "Load failed" on first click
+export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  auth: { persistSession: false }
+})
