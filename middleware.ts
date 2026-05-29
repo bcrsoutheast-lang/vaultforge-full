8c4ebd6c4604f8f43571ff1d49c1250240dd551b
bcrsoutheast-lang <bcrsoutheast@gmail.com>
2026-05-28 23:19:36 -0400

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
 export async function middleware(request: NextRequest) {
-  // return NextResponse.next() 
-  // just let everything through for now
+  // Temporarily disabled for dev - let everything through
   return NextResponse.next()
 }
