19012304f2f7108cb0d56c8ae8e0ec3bfaba8a34
bcrsoutheast-lang <bcrsoutheast@gmail.com>
2026-05-29 12:22:49 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
-const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '*.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
