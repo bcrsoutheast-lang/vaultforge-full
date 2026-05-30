f649cbb631f2f7c7a15d24cf483de8cf1e06db52
bcrsoutheast-lang <bcrsoutheast@gmail.com>
2026-05-30 10:41:33 -0400

    Update client.ts

--- lib/supabase/client.ts
+++ lib/supabase/client.ts
@@ -1,6 +1,10 @@
-use client' 
+'use client'
 
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
+import { createBrowserClient } from '@supabase/ssr'
 import type { Database } from './types'
 
-export const createClient = () => createClientComponentClient<Database>()
+export const createClient = () =>
+  createBrowserClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  )p
