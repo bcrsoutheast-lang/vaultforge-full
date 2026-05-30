3d664cf69b040a5e53b28b79eb6ba17912da7049
bcrsoutheast-lang <bcrsoutheast@gmail.com>
2026-05-30 17:56:55 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    remotePatterns: [{ protocol: 'https', hostname: '**' }],
   },
 }
 
