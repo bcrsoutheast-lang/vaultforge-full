1a8a6763a2d3cd84071f30e8c37c4663f53ae4b5
bcrsoutheast-lang <bcrsoutheast@gmail.com>
2026-05-30 16:37:37 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Fuck it. Ship anyway. Types don't run your app in production.
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Same for lint errors
     ignoreDuringBuilds: true,
   },
 }
