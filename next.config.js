cbd28576b7fd4120f6b2a7ebb7ce0000f1051d16
bcrsoutheast-lang <bcrsoutheast@gmail.com>
2026-06-04 09:10:29 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
+  experimental: {
+    appDir: true,
   },
 }
 
